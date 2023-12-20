ae10c71e0419d74cae55f75ac47468f53c4bac04
Dimpex <mitkodd2005@gmail.com>
2023-12-21 00:54:15 +0200

    added trusted origins to settings.py

--- KRWebsite/settings.py
+++ KRWebsite/settings.py
@@ -19,6 +19,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', None)
 DEBUG = bool(int(os.getenv('DEBUG', 1)))
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(' ')
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split(' ')
 
 
 # Application definition
