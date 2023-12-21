256f61ea454cebfec4857724a5f0e5e471eb9e62
HP 830 G5 <mitkodd2005@gmail.com>
2023-12-21 21:07:10 +0200

    add a robots.txt file

--- KRWebsite/urls.py
+++ KRWebsite/urls.py
@@ -6,6 +6,8 @@ from django.urls import path, include
 from django.contrib.sitemaps import views as sitemap_views
 from KRWebsite.main.sitemap import AlbumSitemap, PortfolioSitemap, HomeSitemap
 
+from django.views.generic.base import TemplateView
+
 sitemaps = {
     'Home': HomeSitemap,
     'Portfolio': PortfolioSitemap,
@@ -15,5 +17,6 @@ sitemaps = {
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('KRWebsite.main.urls')),
+    path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
     path('sitemap.xml', sitemap_views.sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
